681fb77d3d17c6ea33c6861dc23d54a2720453bc
Dev_Skoll <me@skoll.dev>
2024-09-02 14:34:17 -0400

    Add Cross Origin Resource Sharing

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'graphene_django'
+    'graphene_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -72,6 +74,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blogsite.wsgi.application'
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+    'https://skoll.dev',
+    ]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
